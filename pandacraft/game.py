a55c1011f3053de7ac5b3c692b7e77fe832b7a31
Vlad2345553453 <130825340+Vlad2345553453@users.noreply.github.com>
2023-11-27 10:29:07 +0200

    Add files via upload

---
+++ pandacraft/game.py
@@ -0,0 +1,17 @@
+from direct.showbase.ShowBase import ShowBase
+from mapmanager import MapManager
+from hero import Hero
+
+class Game(ShowBase):
+    def __init__(self):
+        super().__init__()
+        self.land = MapManager()
+        self.land.loadLand("land.txt")
+
+        self.hero=Hero((3,5,1),self.land)
+        
+        base.camLens.setFov(90)
+
+
+
+Game().run()
